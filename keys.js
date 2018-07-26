0b279e2f3ec1789eb20b917080661558b46bcd85
Twoxbradley <jimmybradley32@gmail.com>
2018-07-26 14:59:46 -0700

    still in work

---
+++ keys.js
@@ -0,0 +1,14 @@
+console.log('this is loaded');
+
+exports.twitter = {
+  consumer_key: process.env.xbdBh6R0hk5nzwRSPvdb8fY9W ,
+  consumer_secret: process.env.koARNHhlomln2Xbh21tM1tlBHhd7rxlTrvfcfE4S6s4cOdGIgk,
+  access_token_key: process.env.1021578341265039360-1RkHjwdW1zv8nNf9tWMClU2lxvXZnC,
+  access_token_secret: process.env.lcg7IN91n9gPcV7GI5CCDC3YE5n4cxT0mvUU06euamH4V
+
+};
+
+exports.spotify = {
+  id: process.env.618a177a91da42d2bf975b6ed2489b7d,
+  secret: process.env.ef583e8c841248fe9a1b2ec564f58f7e
+};
